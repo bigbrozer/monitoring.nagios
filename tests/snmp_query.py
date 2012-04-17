303dad801b155485623944a666efd30ea1145a7a
Vincent BESANCON <besancon.vincent@gmail.com>
2012-04-17 18:17:50 +0200

    [Tests] Added a test for SNMP probes.

---
+++ tests/snmp_query.py
@@ -0,0 +1,56 @@
+#!/usr/bin/env python2.7
+# -*- coding: utf-8 -*-
+#===============================================================================
+# Filename      : snmp_query.py
+# Author        : Vincent BESANCON <besancon.vincent@gmail.com>
+# Description   : Test SNMPQuery class.
+#-------------------------------------------------------------------------------
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#===============================================================================
+
+import sys
+from pprint import pprint
+sys.path.insert(0, '..')
+
+from monitoring.nagios.plugin import NagiosPluginSNMP
+
+plugin = NagiosPluginSNMP(version='1.0', description='Test SNMPQuery class')
+
+query_get = plugin.snmp.get(name='1.3.6.1.4.1.1588.2.1.1.1.6.2.1.36.65',
+                        alias='1.3.6.1.4.1.1588.2.1.1.1.6.2.1.37.65',
+                        crc='1.3.6.1.4.1.1588.2.1.1.1.6.2.1.22.65',
+                        index=True,
+)
+pprint(query_get)
+
+print '=' * 80
+
+query_getnext = plugin.snmp.getnext(
+    name='1.3.6.1.4.1.1588.2.1.1.1.6.2.1.36',
+    alias='1.3.6.1.4.1.1588.2.1.1.1.6.2.1.37',
+    crc='1.3.6.1.4.1.1588.2.1.1.1.6.2.1.22',
+    index=True,
+)
+pprint(query_getnext)
+
+print '=' * 80
+
+query_table = plugin.snmp.table(
+    name='1.3.6.1.4.1.1588.2.1.1.1.6.2.1.36',
+    alias='1.3.6.1.4.1.1588.2.1.1.1.6.2.1.37',
+    crc='1.3.6.1.4.1.1588.2.1.1.1.6.2.1.22',
+    primary_key='alias',
+)
+print(query_table)
+print query_table['6/2']['crc']
