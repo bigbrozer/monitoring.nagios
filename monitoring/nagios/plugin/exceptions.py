858032c25afe2d9f06dd927dfe899e974bfd3c42
Vincent BESANCON <besancon.vincent@gmail.com>
2012-06-25 11:51:59 +0200

    Cleaned old reference to NagiosPPT in exceptions module.

--- monitoring/nagios/plugin/exceptions.py
+++ monitoring/nagios/plugin/exceptions.py
@@ -1,46 +1,29 @@
+# -*- coding: utf-8 -*-
 #===============================================================================
-# -*- coding: UTF-8 -*-
-# Module        : exceptions
+# Filename        : exceptions
 # Author        : Vincent BESANCON aka 'v!nZ' <besancon.vincent@gmail.com>
-# Description   : Define class for Nagios events exceptions.
+# Description   : Define class for Nagios events and plugin exceptions.
 #-------------------------------------------------------------------------------
-# This file is part of NagiosPPT (Nagios Python Plugin Template).
-#
-# NagiosPPT is free software: you can redistribute it and/or modify
+# This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
 # the Free Software Foundation, either version 3 of the License, or
 # (at your option) any later version.
 #
-# NagiosPPT is distributed in the hope that it will be useful,
+# This program is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 # GNU General Public License for more details.
 #
 # You should have received a copy of the GNU General Public License
-# along with NagiosPPT.  If not, see <http://www.gnu.org/licenses/>.
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #===============================================================================
 
-""":mod:`nagppt.exceptions` -- This module define class for Nagios events exceptions"""
 
 class NagiosCritical(Exception):
     """Raise to fire a CRITICAL event to Nagios and stop plugin execution.
     
     :param msg: Output message in Nagios
     :type msg: string
-    
-    **Usage**:
-    
-    .. sourcecode:: python
-     
-     from nagppt.exceptions import NagiosCritical
-     raise NagiosCritical("Cannot connect to database !")
-    
-    This will make Nagios show a CRITICAL event with following output::
-    
-     CRITICAL - Cannot connect to database !
-     
-    .. note::
-      The plugin will be stopped after raising exception !
     """
     
     def __init__(self, msg):
@@ -52,20 +35,6 @@ class NagiosWarning(Exception):
     
     :param msg: Output message in Nagios
     :type msg: string
-    
-    **Usage**:
-    
-    .. sourcecode:: python
-     
-     from nagppt.exceptions import NagiosWarning
-     raise NagiosWarning("Free space available is low !")
-    
-    This will make Nagios show a WARNING event with following output::
-    
-     WARNING - Free space available is low !
-     
-    .. note::
-      The plugin will be stopped after raising exception !
     """
     
     def __init__(self, msg):
@@ -77,20 +46,6 @@ class NagiosUnknown(Exception):
     
     :param msg: Output message in Nagios
     :type msg: string
-    
-    **Usage**:
-    
-    .. sourcecode:: python
-     
-     from nagppt.exceptions import NagiosUnknown
-     raise NagiosUnknown("Missing mandatory argument 'hostname !")
-    
-    This will make Nagios show a UNKNOWN event with following output::
-    
-     UNKNOWN - Missing mandatory argument 'hostname !
-     
-    .. note::
-      The plugin will be stopped after raising exception !
     """
     
     def __init__(self, msg):
@@ -102,20 +57,6 @@ class NagiosOk(Exception):
     
     :param msg: Output message in Nagios
     :type msg: string
-    
-    **Usage**:
-    
-    .. sourcecode:: python
-     
-     from nagppt.exceptions import NagiosOk
-     raise NagiosOk("Everything is fine.")
-    
-    This will make Nagios show a OK event with following output::
-    
-     OK - Everything is fine.
-     
-    .. note::
-      The plugin will be stopped after raising exception !
     """
     
     def __init__(self, msg):
@@ -125,6 +66,7 @@ class NagiosOk(Exception):
 class PluginError(StandardError):
     """
     Exception when a plugin error occur.
+
     :param output: Message to show in Nagios status information output.
     :type output: str
     :param longoutput: Message to show in long output (extra infos).
