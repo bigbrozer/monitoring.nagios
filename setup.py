ee5f871ad4e273e25ba6ff7fab9bc2d935763aaf
Vincent BESANCON <besancon.vincent@gmail.com>
2012-06-25 11:53:11 +0200

    Setup: specify exact version of dependencies for Cython and pymssql packages.

--- setup.py
+++ setup.py
@@ -33,8 +33,8 @@ setup(name = 'monitoring.nagios',
     install_requires = [
         'pysnmp>=4.1',
         'ssh>=1.7.13',
-        'Cython',
-        'pymssql',
+        'Cython>=0.15',
+        'pymssql>=1.0.2',
     ],
     extras_require = {
         'graph': ['pygraphviz>=1.0'],
