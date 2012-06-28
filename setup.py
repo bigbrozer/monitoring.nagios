5cb7752b33e2d6c297d87ae807a02a9bd8755283
Vincent BESANCON <besancon.vincent@gmail.com>
2012-06-28 17:00:11 +0200

    Setuptools: added ia detection. Do not install same version of pymssql on 64 bits and 32 bits servers.

--- setup.py
+++ setup.py
@@ -19,9 +19,26 @@
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #===============================================================================
 
+import os
 from setuptools import setup, find_packages
 import monitoring.nagios
 
+# Package dependencies
+dependencies = [
+    'pysnmp>=4.1',
+    'ssh>=1.7.13',
+    'Cython>=0.15',
+]
+
+# OS dependent
+if 'x86_64' in os.uname()[-1]:
+    # We are in 64 bits
+    dependencies.append('pymssql >1.0.2')
+else:
+    # We are in 32 bits
+    dependencies.append('pymssql ==1.0.2')
+
+# Init distribute
 setup(name = 'monitoring.nagios',
     version = monitoring.nagios.__version__,
     description = 'Monitoring Python Package',
@@ -30,12 +47,7 @@ setup(name = 'monitoring.nagios',
     license = 'GPL',
     namespace_packages = ['monitoring'],
     packages = find_packages(),
-    install_requires = [
-        'pysnmp>=4.1',
-        'ssh>=1.7.13',
-        'Cython>=0.15',
-        'pymssql==1.0.2',
-    ],
+    install_requires = dependencies,
     extras_require = {
         'graph': ['pygraphviz>=1.0'],
     },
