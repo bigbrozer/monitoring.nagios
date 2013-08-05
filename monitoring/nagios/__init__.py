420cc48e3ddc0a130ee77bc4143d4105f3e248a9
Vincent BESANCON <besancon.vincent@gmail.com>
2013-08-05 12:01:09 +0200

    Fixed pymssql version detection for pip 1.4.x. refs #2529

--- monitoring/nagios/__init__.py
+++ monitoring/nagios/__init__.py
@@ -1,3 +1,3 @@
 import monitoring.nagios.logger
 
-__version__ = '1.2.2'
+__version__ = '1.2.3'
--- setup.py
+++ setup.py
@@ -33,10 +33,10 @@ dependencies = [
 # OS dependent
 if 'x86_64' in os.uname()[-1]:
     # We are in 64 bits
-    dependencies.append('pymssql >1.0.2')
+    dependencies.append('pymssql==2.0.0b1-dev-20130403')
 else:
     # We are in 32 bits
-    dependencies.append('pymssql ==1.0.2')
+    dependencies.append('pymssql==1.0.2')
 
 # Init distribute
 setup(name = 'monitoring.nagios',
