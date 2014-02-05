ae43b6c5378e049b02835856bd0afd6dab076e5c
Vincent BESANCON <besancon.vincent@gmail.com>
2014-02-05 23:19:21 +0100

    Refresh fabfile and fix pylint+pep8.

--- fabfile.py
+++ fabfile.py
@@ -1,24 +1,23 @@
 # -*- coding: UTF-8 -*-
-#===============================================================================
-# Module        : fabfile
-# Author        : Vincent BESANCON aka 'v!nZ' <besancon.vincent@gmail.com>
-# Description   : Fabric tasks to manage the project.
-#-------------------------------------------------------------------------------
-# This file is part of NagiosPPT (Nagios Python Plugin Template).
+# Copyright (C) Vincent BESANCON <besancon.vincent@gmail.com>
 #
-# NagiosPPT is free software: you can redistribute it and/or modify
-# it under the terms of the GNU General Public License as published by
-# the Free Software Foundation, either version 3 of the License, or
-# (at your option) any later version.
+# Permission is hereby granted, free of charge, to any person obtaining
+# a copy of this software and associated documentation files (the "Software"),
+# to deal in the Software without restriction, including without limitation
+# the rights to use, copy, modify, merge, publish, distribute, sublicense,
+# and/or sell copies of the Software, and to permit persons to whom the
+# Software is furnished to do so, subject to the following conditions:
 #
-# NagiosPPT is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU General Public License for more details.
+# The above copyright notice and this permission notice shall be included
+# in all copies or substantial portions of the Software.
 #
-# You should have received a copy of the GNU General Public License
-# along with NagiosPPT.  If not, see <http://www.gnu.org/licenses/>.
-#===============================================================================
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+# EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES
+# OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+# IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM,
+# DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT,
+# TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE
+# OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 
 """
 Fabric tasks for project monitoring.nagios.
@@ -26,19 +25,16 @@ Fabric tasks for project monitoring.nagios.
 
 import os
 
-from fabric.api import *
+from fabric.api import (
+    env,
+    task,
+    hosts,
+    local,
+)
 from fabric.contrib.project import rsync_project
 from monitoring.fabric import servers
 
-#-------------------------------------------------------------------------------
-@task
-@roles('satellites', 'satellite_dev', 'satellite_tpl')
-def install():
-    """Install / upgrade package on all Nagios satellites."""
-    sudo('pip uninstall -y monitoring.nagios')
-    sudo('http_proxy=\"http://monitoring-dc.app.corp:8080/\" pip install --upgrade git+http://monitoring-dc.app.corp/git/lib/monitoring.nagios.git')
 
-#-------------------------------------------------------------------------------
 @task
 @hosts('monitoring-dc.app.corp')
 def doc():
@@ -50,4 +46,3 @@ def doc():
 
     local('make doc')
     rsync_project(DOCDIR, 'docs/_build/html/', delete=True)
-
