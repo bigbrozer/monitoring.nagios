272a49bd29e1f546e6dc4a2f3f53ade4bbac9b62
Vincent BESANCON <besancon.vincent@gmail.com>
2014-02-06 15:45:49 +0100

    Refresh monitoring.nagios.plugin.secureshell and fix pylint+pep8.

--- monitoring/nagios/plugin/secureshell.py
+++ monitoring/nagios/plugin/secureshell.py
@@ -1,22 +1,25 @@
 # -*- coding: utf-8 -*-
-#===============================================================================
-# Filename      : monitoring.nagios.plugin.secureshell
-# Author        : Vincent BESANCON <besancon.vincent@gmail.com>
-# Description   : Class to define a standard Nagios SSH plugin.
-#-------------------------------------------------------------------------------
-# This program is free software: you can redistribute it and/or modify
-# it under the terms of the GNU General Public License as published by
-# the Free Software Foundation, either version 3 of the License, or
-# (at your option) any later version.
+# Copyright (C) Vincent BESANCON <besancon.vincent@gmail.com>
 #
-# This program is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU General Public License for more details.
+# Permission is hereby granted, free of charge, to any person obtaining
+# a copy of this software and associated documentation files (the "Software"),
+# to deal in the Software without restriction, including without limitation
+# the rights to use, copy, modify, merge, publish, distribute, sublicense,
+# and/or sell copies of the Software, and to permit persons to whom the
+# Software is furnished to do so, subject to the following conditions:
 #
-# You should have received a copy of the GNU General Public License
-# along with this program.  If not, see <http://www.gnu.org/licenses/>.
-#===============================================================================
+# The above copyright notice and this permission notice shall be included
+# in all copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+# EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES
+# OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+# IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM,
+# DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT,
+# TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE
+# OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+"""SSH module for plugins."""
 
 import logging as log
 import os
@@ -30,8 +33,8 @@ logger = log.getLogger('monitoring.nagios.plugin.ssh')
 
 class NagiosPluginSSH(NagiosPlugin):
     """Base for a standard SSH Nagios plugin"""
-
-    def __init__(self, name=os.path.basename(sys.argv[0]), version='', description=''):
+    def __init__(self, name=os.path.basename(sys.argv[0]), version='',
+                 description=''):
         super(NagiosPluginSSH, self).__init__(name, version, description)
 
         # Init a new probe of type SSH
@@ -42,23 +45,38 @@ class NagiosPluginSSH(NagiosPlugin):
             password=self.options.password,
         )
 
-        if 'NagiosPluginSSH' == self.__class__.__name__: logger.debug('=== END PLUGIN INIT ===')
+        if 'NagiosPluginSSH' == self.__class__.__name__:
+            logger.debug('=== END PLUGIN INIT ===')
 
     def define_plugin_arguments(self):
         """Define arguments for the plugin"""
-        # Define common arguments
         super(NagiosPluginSSH, self).define_plugin_arguments()
 
         # Add extra arguments
-        self.parser.add_argument('-u', '--username', dest='username', default=None,
-                                 help='Login user. Default to current logged in user.', required=False)
-        self.parser.add_argument('-p', '--password', dest='password', default=None,
-                                 help='Login user password. Default is to use pub key of the current user.',
+        self.parser.add_argument('-u', '--username',
+                                 dest='username',
+                                 default=None,
+                                 help='Login user. Default to current logged '
+                                      'in user.',
                                  required=False)
-        self.parser.add_argument('-P', type=int, dest='port', default=22, help='Port to connect to (default to 22).')
-        self.parser.add_argument('-t', '--timeout', type=float, dest='timeout', default=10, help='Connection timeout in seconds (default to 10 secs).')
+        self.parser.add_argument('-p', '--password',
+                                 dest='password',
+                                 default=None,
+                                 help='Login user password. Default is to use '
+                                      'pub key of the current user.',
+                                 required=False)
+        self.parser.add_argument('-P',
+                                 type=int,
+                                 dest='port',
+                                 default=22,
+                                 help='Port to connect to (default to 22).')
+        self.parser.add_argument('-t', '--timeout',
+                                 type=float,
+                                 dest='timeout',
+                                 default=10,
+                                 help='Connection timeout in seconds (default '
+                                      'to 10 secs).')
 
     def verify_plugin_arguments(self):
         """Check syntax of all arguments"""
-        # Check common arguments syntax
         super(NagiosPluginSSH, self).verify_plugin_arguments()
